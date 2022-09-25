6ae7655e912b8abf13d51f0357c438379fdf77a5
btchoum <bertrand.tchoumkeu@gmail.com>
2022-09-25 12:39:36 -0500

    Fix tags

--- variables.tf
+++ variables.tf
@@ -7,11 +7,3 @@ variable "app_service_sku" {
   description = "App Service SKU Name"
   default     = "F1"
 }
-
-variable "tags" {
-  type = map
-
-  default = {
-    Environment = terraform.workspace
-  }
-}
